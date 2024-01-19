cff3893603fe1e4aee44033ce5c1427c0e016302
dshaulov1 <dshaulov@cse-healthcare.io>
2024-01-19 02:31:33 +0100

    Correct file structure

--- planroute/settings.py
+++ planroute/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k!+k7wwnq&51#3h=q-5aw3o^a%njef5!m=#hrvjj%un#qmm$4p'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
