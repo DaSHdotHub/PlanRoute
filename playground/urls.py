418679f608661f0e10f52b40d82d161060af2e86
dshaulov1 <dshaulov@cse-healthcare.io>
2024-01-21 04:52:02 +0100

    Remove playground from project

--- playground/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import patient_list
-
-urlpatterns = [
-    # ... other url patterns ...
-    path('', patient_list, name='patient-list'),
-]
--- playground/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Patient
-
-#Create your views here.
-def patient_list(request):
-    patients = Patient.objects.all()
-    return render(request, 'patient_list.html', {'patients': patients})
