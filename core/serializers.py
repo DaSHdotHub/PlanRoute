8223824f8b7964252c1db8b6bc672ff2233bebaa
dshaulov1 <dshaulov@cse-healthcare.io>
2024-01-21 17:20:28 +0100

    US4.1: Update AddressSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,26 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.response import Response
 from .models import Patient, Address
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = '__all__'
+
+    def create(self, validated_data):
+        address, created = Address.objects.get_or_create(
+            street=validated_data['street'],
+            street_number=validated_data['street_number'],
+            zip_code=validated_data['zip_code'],
+            city=validated_data['city'],
+            country=validated_data['country'],
+            defaults=validated_data
+        )
+        if created:
+            return Response({'message': 'New address created.', 'address': address}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Address already exists.', 'address': address}, status=status.HTTP_200_OK)
+
 
 class PatientSerializer(serializers.ModelSerializer):
     address = AddressSerializer()
