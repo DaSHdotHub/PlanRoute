4ff340c09aa7dccf6fc73f468a639d9684c61a94
dshaulov1 <dshaulov@cse-healthcare.io>
2024-01-21 04:54:49 +0100

    US4.1: set url in project for core

--- planroute/urls.py
+++ planroute/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
-    path('', RedirectView.as_view(url='playground/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    path('core/', include('core.urls')),
+    ]
