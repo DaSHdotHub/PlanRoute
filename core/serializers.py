04085f8c94aa5220d723da3b0c0dbc331bd16c46
dshaulov1 <dshaulov@cse-healthcare.io>
2024-01-21 18:17:47 +0100

    US4.1: Move get_or_create logic to views

--- core/serializers.py
+++ core/serializers.py
@@ -1,26 +1,10 @@
-from rest_framework import serializers, status
-from rest_framework.response import Response
+from rest_framework import serializers
 from .models import Patient, Address
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = '__all__'
-
-    def create(self, validated_data):
-        address, created = Address.objects.get_or_create(
-            street=validated_data['street'],
-            street_number=validated_data['street_number'],
-            zip_code=validated_data['zip_code'],
-            city=validated_data['city'],
-            country=validated_data['country'],
-            defaults=validated_data
-        )
-        if created:
-            return Response({'message': 'New address created.', 'address': address}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({'message': 'Address already exists.', 'address': address}, status=status.HTTP_200_OK)
-
+        fields = ['id', 'street', 'street_number', 'zip_code', 'city', 'country', 'latitude', 'longitude']
 
 class PatientSerializer(serializers.ModelSerializer):
     address = AddressSerializer()
@@ -28,18 +12,3 @@ class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = ['id', 'gender', 'firstname', 'lastname', 'birth_date', 'address', 'creator', 'last_editor']
-
-    def create(self, validated_data):
-        address_data = validated_data.pop('address')
-        # Check if an address with these details already exists
-        address, created = Address.objects.get_or_create(
-            street=address_data['street'],
-            street_number=address_data['street_number'],
-            zip_code=address_data['zip_code'],
-            city=address_data['city'],
-            country=address_data['country'],
-            defaults=address_data
-        )
-        # Create the patient with the existing or new address
-        patient = Patient.objects.create(address=address, **validated_data)
-        return patient
--- core/views.py
+++ core/views.py
@@ -1,11 +1,62 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Patient, Address
 from .serializers import PatientSerializer, AddressSerializer
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        address_serializer = AddressSerializer(data=request.data.get('address'))
+        if address_serializer.is_valid(raise_exception=True):
+            address_data = address_serializer.validated_data
+            address, address_created = Address.objects.get_or_create(**address_data)
+
+            patient_data = serializer.validated_data
+            patient_data['address'] = address
+            patient, patient_created = Patient.objects.get_or_create(
+                firstname=patient_data['firstname'],
+                lastname=patient_data['lastname'],
+                birth_date=patient_data.get('birth_date', None),
+                defaults={
+                    'address': address,
+                    'gender': patient_data.get('gender', ''),
+                    'creator': patient_data.get('creator'),
+                    'last_editor': patient_data.get('last_editor')
+                }
+            )
+
+            if patient_created:
+                headers = self.get_success_headers(serializer.data)
+                return Response(patient.id, status=status.HTTP_201_CREATED, headers=headers)
+            else:
+                return Response({"message": "Patient already exists", "id": patient.id}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_BAD_REQUEST)
     
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        address, created = Address.objects.get_or_create(
+            street=serializer.validated_data['street'],
+            street_number=serializer.validated_data['street_number'],
+            zip_code=serializer.validated_data['zip_code'],
+            city=serializer.validated_data['city'],
+            country=serializer.validated_data['country'],
+            defaults=serializer.validated_data
+        )
+
+        if created:
+            headers = self.get_success_headers(serializer.data)
+            return Response(address.id, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response({"message": "Address already exists", "id": address.id}, status=status.HTTP_200_OK)
