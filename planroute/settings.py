ceea1c67a49ead5aa1b3b6f5f9be601f5b5938d9
Daniil <dshaulov@outlook.com>
2024-02-12 01:19:53 +0100

    bugfixing

--- planroute/settings.py
+++ planroute/settings.py
@@ -38,7 +38,7 @@ if DEVELOPMENT:
         '127.0.0.1',
         'localhost',
         ]
-    CORS_ALLOW_ALL_ORIGINS = False
+    CORS_ALLOW_ALL_ORIGINS = True
     CORS_ALLOWED_ORIGINS = ['http://localhost:8080', 'http://127.0.0.1:8000']
     CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com", "http://localhost:8080", "http://127.0.0.1:8000", "https://*.ondigitalocean.app"]
 else:
@@ -158,10 +158,6 @@ SIMPLE_JWT = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# PATHS TO STATIC FILES, VUE App
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist'),
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
