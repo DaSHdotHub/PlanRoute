79be1d873ae1a84ca307efb5536eb163fe0b6a48
dshaulov1 <dshaulov@cse-healthcare.io>
2024-02-13 18:41:47 +0100

    Update dev_mode variable to use os.getenv() instead of os.environ.get()

--- core/urls.py
+++ core/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
 ]
 
 # Add the Swagger and Redoc documentation for the API in development mode
-dev_mode = os.environ.get("DEVELOPMENT")
+dev_mode = os.getenv("DEVELOPMENT", "False") == "True"
 if dev_mode:
     urlpatterns += [
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
