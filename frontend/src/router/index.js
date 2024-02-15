285693e0a4577c0bb30b9e071a3a5afc6d513540
dshaulov1 <dshaulov@cse-healthcare.io>
2024-02-15 07:37:17 +0100

    Return to WebHistory

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import AboutPage from "@/views/AboutPage.vue";
 import LandingPage from "@/views/LandingPage.vue";
 import UserDashboard from "@/views/UserDashboard.vue";
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
