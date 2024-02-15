758bd5827e12a047ab5114df419f1da1d3e53ae8
dshaulov1 <dshaulov@cse-healthcare.io>
2024-02-15 07:31:14 +0100

    Add is_editor to Usermodel

--- core/serializers.py
+++ core/serializers.py
@@ -2,26 +2,49 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Patient, Address, Distance
 
+
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ['id', 'street', 'street_number', 'zip_code', 'city', 'country', 'latitude', 'longitude']
+        fields = [
+            "id",
+            "street",
+            "street_number",
+            "zip_code",
+            "city",
+            "country",
+            "latitude",
+            "longitude",
+        ]
+
 
 class PatientSerializer(serializers.ModelSerializer):
     address = AddressSerializer()
 
     class Meta:
         model = Patient
-        fields = ['id', 'gender', 'firstname', 'lastname', 'birth_date', 'address', 'creator', 'last_editor']
-        
+        fields = [
+            "id",
+            "gender",
+            "firstname",
+            "lastname",
+            "birth_date",
+            "address",
+            "creator",
+            "last_editor",
+        ]
+
+
 class DistanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Distance
-        fields = '__all__'
-        
+        fields = "__all__"
+
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    is_editor = serializers.BooleanField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ("username", "password", "email", "is_editor")
